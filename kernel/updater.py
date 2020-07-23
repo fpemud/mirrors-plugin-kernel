959dfd492c24e23d90845a4fd3217529d8f04a4c
fpemud <fpemud@sina.com>
2020-07-23 11:37:09 +0800

    fix

--- kernel/updater.py
+++ kernel/updater.py
@@ -1,39 +1,44 @@
 #!/usr/bin/python3
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 
-import os
 import sys
 import time
+import json
 import subprocess
 
 
-def run():
-    url = "http://mirrors.tuna.tsinghua.edu.cn/kernel"
-    dataDir = sys.argv[1]
-    logDir = sys.argv[2]
-    logFile = os.path.join(logDir, "wget.log")
-    cmd = "/usr/bin/wget -m --no-parent -e robots=off -nH --cut-dirs=1 --wait 1 --reject \"index.html\" -P \"%s\" %s >%s 2>&1" % (dataDir, url, logFile)
-    _Util.shellCall(cmd)
+def main():
+    dataDir = json.loads(sys.argv[1])["storage-file"]["data-directory"]
+    rsyncSource = "rsync://rsync.kernel.org/pub"
+    _Util.cmdExec("/usr/bin/rsync", "-v", "-a", "-z", "--delete", rsyncSource, dataDir)
 
 
 class _Util:
 
     @staticmethod
-    def shellCall(cmd):
-        # call command with shell to execute backstage job
-        # scenarios are the same as FmUtil.cmdCall
+    def cmdExec(cmd, *kargs):
+        # call command to execute frontend job
+        #
+        # scenario 1, process group receives SIGTERM, SIGINT and SIGHUP:
+        #   * callee must auto-terminate, and cause no side-effect
+        #   * caller must be terminate AFTER child-process, and do neccessary finalization
+        #   * termination information should be printed by callee, not caller
+        # scenario 2, caller receives SIGTERM, SIGINT, SIGHUP:
+        #   * caller should terminate callee, wait callee to stop, do neccessary finalization, print termination information, and be terminated by signal
+        #   * callee does not need to treat this scenario specially
+        # scenario 3, callee receives SIGTERM, SIGINT, SIGHUP:
+        #   * caller detects child-process failure and do appopriate treatment
+        #   * callee should print termination information
 
-        ret = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                             shell=True, universal_newlines=True)
+        # FIXME, the above condition is not met, FmUtil.shellExec has the same problem
+
+        ret = subprocess.run([cmd] + list(kargs), universal_newlines=True)
         if ret.returncode > 128:
-            # for scenario 1, caller's signal handler has the oppotunity to get executed during sleep
             time.sleep(1.0)
-        if ret.returncode != 0:
-            ret.check_returncode()
-        return ret.stdout.rstrip()
+        ret.check_returncode()
 
 
 ###############################################################################
 
 if __name__ == "__main__":
-    run()
+    main()
